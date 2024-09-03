c3fe62bf3c4f4392b7d1d57468974f73ed83a852
Jaison T Poulose <jaisontpoulose@gmail.com>
2024-09-03 17:14:01 +0530

    changes updated

---
+++ script.js
@@ -0,0 +1,25 @@
+Shery.mouseFollower();
+Shery.hoverWithMediaCircle(".profile_name",{videos: ["./1.mp4"]});
+Shery.makeMagnet(".magnet");
+Shery.makeMagnet(".skill-imag");
+Shery.imageEffect(".imgg", {
+    style: 3,
+    /*optional parameters
+    these parameter dose not applies to custom scroll trigger callback */
+    scrollSnapping: true,
+    scrollSpeed: 7,
+    touchSpeed: 6,
+    damping: 7,
+  });
+
+//   Shery.makeMagnet(".pic1");
+//   Shery.makeMagnet(".pic2");
+
+//   Shery.makeMagnet(".pic3");
+
+  
+  
+
+
+
+
